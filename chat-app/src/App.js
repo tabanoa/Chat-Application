6e8b9b17b630588c0f5c300a50e6ff34a0e4371f
Manjot Sidhu <manjot.sidhuu98@gmail.com>
2021-05-19 16:16:56 -0400

    Populating chat functionality added

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,4 +1,5 @@
 import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
@@ -8,8 +9,9 @@ const App = () => {
       projectID="f0f041e0-40ae-4ba6-8432-c0e55a99b6c4"
       userName="manjot123"
       userSecret="manjot123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
-  )
+  );
 }
 
 export default App;
