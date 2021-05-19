be70d80776bc827e54fe3b44050799540330331b
Manjot Sidhu <manjot.sidhuu98@gmail.com>
2021-05-19 19:55:37 -0400

    MyMessage implemented

--- chat-app/src/components/ChatFeed.js
+++ chat-app/src/components/ChatFeed.js
@@ -23,8 +23,8 @@ const ChatFeed = (props) => {
                     <div className="message-block">
                      {
                          isMyMessage 
-                         ? <MyMessage /> 
-                         : <TheirMessage />
+                         ? <MyMessage message={message} /> 
+                         : <TheirMessage message={message} lastMessageKey={messages[lastMessageKey]}/>
                      }
                      </div>
                      <div className="read-receipts" style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
