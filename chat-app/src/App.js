b780cd9acba8fd56eaedb8592743ada8b3998842
Manjot Sidhu <manjot.sidhuu98@gmail.com>
2021-05-19 15:54:47 -0400

    Chat settings authenticated with chat engine

---
+++ chat-app/src/App.js
@@ -0,0 +1,15 @@
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="f0f041e0-40ae-4ba6-8432-c0e55a99b6c4"
+      userName="manjot123"
+      userSecret="manjot123"
+    />
+  )
+}
+
+export default App;
