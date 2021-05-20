17e854659054687ca4393ab45fafd598267efed9
Manjot Sidhu <manjot.sidhuu98@gmail.com>
2021-05-20 13:40:03 -0400

    Login authentications implemented

--- chat-app/src/components/LoginForm.jsx
+++ chat-app/src/components/LoginForm.jsx
@@ -1,23 +1,22 @@
-import {useState} from 'react-redux';
+import {useState} from 'react';
 import axios from 'axios';
 
 const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const authObj = { 'Project-ID': "f0f041e0-40ae-4ba6-8432-c0e55a99b6c4", 
-                          'User-Name': username,
-                          'User-Secret': password  }
-        try{
-            axios.get('http://api.chat_engine.io/chats', {headers: authObj});
+    const authObject = { 'Project-ID': "f0f041e0-40ae-4ba6-8432-c0e55a99b6c4", 'User-Name': username, 'User-Secret': password };
 
-            localStorage.setItem('username', username);
-            localStorage.setItem('password', password);
+    try {
+      await axios.get('https://api.chatengine.io/chats', { headers: authObject });
 
-            window.location.reload();
+      localStorage.setItem('username', username);
+      localStorage.setItem('password', password);
+
+      window.location.reload();
         }catch(error) {
 
         }
@@ -28,16 +27,18 @@ const LoginForm = () => {
             <div className="form">
                 <h1 className='title'>Chat Application</h1>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" value={username} onChange={(e) => setUsername(e,target.value)}
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}
                     className="input" placeholder="Username" required />
-                    <input type="text" value={password} onChange={(e) => setPassword(e,target.value)}
+                    <input type="text" value={password} onChange={(e) => setPassword(e.target.value)}
                     className="input" placeholder="Password" required />
                     <div align="center">
-                        <buuton type="submit" className="button"></buuton>
-                            <span>Start Chating</span>
+                        <button type="submit" className="button"></button>
+                            <span>Start Chatting</span>
                     </div>
                 </form>
             </div>
         </div>
-    )
+    );
 }
+
+export default LoginForm;
