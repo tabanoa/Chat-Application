07939dc2510f1488da512de218e8f84d6c2a159a
Manjot Sidhu <manjot.sidhuu98@gmail.com>
2021-05-19 20:27:19 -0400

    Message form created

--- chat-app/src/components/MessageForm.jsx
+++ chat-app/src/components/MessageForm.jsx
@@ -1,8 +1,24 @@
+import {useState} from 'react';
 const MessageForm = () => {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = () => {
+
+    }
+
+    const handleChange = () => {
+
+    } 
+
     return (
-        <div>
-            MessageForm
-        </div>
+        <form className="message-form" onsubmit={handleSubmit}>
+            <input
+                className="message-input"
+                placeholder="Send a message"
+                value={value}
+                onChange={handleChange}
+                onSubmit = {handleSubmit}/>
+        </form>
     );
 }
 
